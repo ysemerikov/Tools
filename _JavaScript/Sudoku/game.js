3ebaf991cce7ce1f3c2c88869d2f9acad161d6fa
Yury Semerikov <ysemerikov@servicetitan.com>
2019-02-12 18:25:29 -0800

    JS sudoku checking

--- _JavaScript/Sudoku/game.js
+++ _JavaScript/Sudoku/game.js
@@ -3,8 +3,10 @@ class Sudoku {
         this.FreezeAllowed = ko.observable(true);
         this.SaveLoadInProcess = ko.observable(false);
         this.SerializedValue = ko.observable('');
+        this.HasErrors = ko.observable(false);
 
         this.Field = Sudoku._createField();
+        this._cellSets = Sudoku._createCellSets(this.Field);
     }
 
     static _createField() {
@@ -20,7 +22,7 @@ class Sudoku {
         return field;
     }
 
-    static _fromSymbolToValue(s) {
+    static fromSymbolToValue(s) {
         if ('0' < s && s <= '9')
             s -= '0';
 
@@ -28,6 +30,28 @@ class Sudoku {
             return s;
 
         return 0;
+    }
+
+    static _createCellSets(field) {
+        let result = {Rows: [], Cols: [], Squares: undefined};
+        let squares = new Array(9).fill(0).map(x => []);
+
+        for (let i = 0; i < 9; i++) {
+            let row = [];
+            let col = [];
+            for (let j = 0; j < 9; ++j) {
+                row.push(field[i][j]);
+                col.push(field[j][i]);
+                
+                let squareNumber = Math.floor(i/3) + 3 * Math.floor(j/3);
+                squares[squareNumber].push(field[i][j]);
+            }
+            result.Rows.push(new cellSet(row));
+            result.Cols.push(new cellSet(col));
+        }
+
+        result.Squares = squares.map(x => new cellSet(x));
+        return result;
     }
 
     Freeze() {
@@ -39,7 +63,7 @@ class Sudoku {
         for (let x = 0; x < 9; ++x) {
             for (let y = 0; y < 9; ++y) {
                 let cell = this.Field[x][y];
-                if (Sudoku._fromSymbolToValue(cell.Value()) > 0)
+                if (Sudoku.fromSymbolToValue(cell.Value()) > 0)
                     cell.IsEditable(false);
             }
         }
@@ -49,7 +73,7 @@ class Sudoku {
         let serializedValue = '';
         for (let x = 0; x < 9; ++x) {
             for (let y = 0; y < 9; ++y) {
-                let value = Sudoku._fromSymbolToValue(this.Field[x][y].Value());
+                let value = Sudoku.fromSymbolToValue(this.Field[x][y].Value());
                 serializedValue += value;
             }
         }
@@ -66,12 +90,57 @@ class Sudoku {
         let i = 0;
         for (let x = 0; x < 9; ++x) {
             for (let y = 0; y < 9; ++y) {
-                let value = Sudoku._fromSymbolToValue(serializedValue[i++]);
+                let value = Sudoku.fromSymbolToValue(serializedValue[i++]);
                 this.Field[x][y].Value(value === 0 ? '' : value);
             }
         }
 
         this.SaveLoadInProcess(false);
+    }
+
+    Check() {
+        let isRowsGood = this._cellSets.Rows.map(x => x.check()).every(x => x === true);
+        let isColsGood = this._cellSets.Cols.map(x => x.check()).every(x => x === true);
+        let isSquaresGood = this._cellSets.Squares.map(x => x.check()).every(x => x === true);
+        let isGood = isRowsGood && isColsGood && isSquaresGood;
+        this.HasErrors(!isGood);
+        if (isGood)
+            alert('Great!');
+    }
+    
+    HideErrors() {
+        if (!this.HasErrors())
+            return;
+
+        for (let x = 0; x < 9; ++x)
+            for (let y = 0; y < 9; ++y)
+                this.Field[x][y].IsError(false);
+            
+        this.HasErrors(false);
+    }
+}
+
+class cellSet {
+    constructor(cells) {
+        this._cells = cells;
+    }
+
+    check() {
+        let hs = {};
+        for (let i = 0; i < this._cells.length; ++i) {
+            let cell = this._cells[i];
+            let value = Sudoku.fromSymbolToValue(cell.Value());
+
+            if (value === 0)
+            {
+                cell.IsError(true);
+            } else if (hs[value] === undefined) {
+                hs[value] = cell;
+            } else {
+                hs[value].IsError(true);
+                cell.IsError(true);
+            }
+        }
     }
 }
 
