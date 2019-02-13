1af3bd00c07b410a186c746996ec595b3f6f802e
Yury Semerikov <ysemerikov@servicetitan.com>
2019-02-12 17:41:27 -0800

    JS sudoku saving

--- _JavaScript/Sudoku/game.js
+++ _JavaScript/Sudoku/game.js
@@ -1,6 +1,8 @@
 class Sudoku {
     constructor() {
         this.FreezeAllowed = ko.observable(true);
+        this.SaveLoadInProcess = ko.observable(false);
+        this.SerializedValue = ko.observable('');
 
         this.Field = Sudoku._createField();
     }
@@ -8,7 +10,7 @@ class Sudoku {
     static _createField() {
         let field = new Array(9);
 
-        for (let x = 0; x < field.length; ++x) {
+        for (let x = 0; x < 9; ++x) {
             field[x] = new Array(9);
             for (let y = 0; y < 9; ++y) {
                 field[x][y] = new cell(x, y);
@@ -25,7 +27,7 @@ class Sudoku {
         if (0 < s && s <= 9)
             return s;
 
-        return undefined;
+        return 0;
     }
 
     Freeze() {
@@ -33,6 +35,43 @@ class Sudoku {
             return;
 
         this.FreezeAllowed(false);
+
+        for (let x = 0; x < 9; ++x) {
+            for (let y = 0; y < 9; ++y) {
+                let cell = this.Field[x][y];
+                if (Sudoku._fromSymbolToValue(cell.Value()) > 0)
+                    cell.IsEditable(false);
+            }
+        }
+    }
+
+    SaveLoad() {
+        let serializedValue = '';
+        for (let x = 0; x < 9; ++x) {
+            for (let y = 0; y < 9; ++y) {
+                let value = Sudoku._fromSymbolToValue(this.Field[x][y].Value());
+                serializedValue += value;
+            }
+        }
+
+        this.SerializedValue(serializedValue);
+        this.SaveLoadInProcess(!this.SaveLoadInProcess());
+    }
+
+    Load() {
+        if (!this.SaveLoadInProcess())
+            return;
+
+        let serializedValue = this.SerializedValue().replace(/\D+/g, '');
+        let i = 0;
+        for (let x = 0; x < 9; ++x) {
+            for (let y = 0; y < 9; ++y) {
+                let value = Sudoku._fromSymbolToValue(serializedValue[i++]);
+                this.Field[x][y].Value(value === 0 ? '' : value);
+            }
+        }
+
+        this.SaveLoadInProcess(false);
     }
 }
 
